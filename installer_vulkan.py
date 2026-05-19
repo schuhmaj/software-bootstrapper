1ad06b00862d7b9673273a20db47cb17a5b3e3c1
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2026-05-19 16:10:13 +0200

    update vulkan installer

--- installer_vulkan.py
+++ installer_vulkan.py
@@ -5,17 +5,31 @@ from pathlib import Path
 import urllib.request
 import tarfile
 import platform
+import shutil
 from typing import Literal
 
-VULKAN_SDK_VERSION = "1.4.328.1"
+VULKAN_SDK_VERSION: str = "1.4.350.0"
+SLANG_VERSION: str = "2026.9"
+
+ARCHITECTURE: str = platform.machine()
+SYSTEM: str = platform.system()
+OS_VERSION: str = platform.version()
 
 
 def get_download_url():
-    arch = platform.machine()
-    system = platform.system()
-    if arch != "x86_64" and system != "Linux":
-        raise RuntimeError("Only Linux x86_64 is supported with this installer script!")
-    return f"https://sdk.lunarg.com/sdk/download/{VULKAN_SDK_VERSION}/linux/vulkansdk-linux-x86_64-{VULKAN_SDK_VERSION}.tar.xz"
+
+    if ARCHITECTURE == "x86_64" and SYSTEM == "Linux":
+        # Official Vulkan SDK download page
+        print("Downloading from official Vulkan SDK download page for x86_64 Linux")
+        return f"https://sdk.lunarg.com/sdk/download/{VULKAN_SDK_VERSION}/linux/vulkansdk-linux-x86_64-{VULKAN_SDK_VERSION}.tar.xz"
+    elif ARCHITECTURE == "aarch64" and SYSTEM == "Linux" and "Ubuntu" in OS_VERSION:
+        # Unofficial, but public build of the VulkanSDK for aarch64 in a GitHub CI Runner for Ubuntu 22.04
+        print("Downloading from unofficial GitHub CI Runner for aarch64 Ubuntu 22.04")
+        return f"https://github.com/jakoch/vulkan-sdk-arm/releases/download/{VULKAN_SDK_VERSION}/vulkansdk-ubuntu-22.04-arm-{VULKAN_SDK_VERSION}.tar.xz"
+    else:
+        raise RuntimeError(
+            "The Script at the moment only supports Linux x86_64 and Ubuntu aarch64 systems. Please built the VulkanSDK from source yourself (maybe in a Docker Image)"
+        )
 
 
 def install_vulkan(
@@ -35,7 +49,42 @@ def install_vulkan(
             with tarfile.open(archive_path) as tar:
                 tar.extractall(install_dir)
 
-    return install_dir / vulkan_version / "x86_64"
+    return install_dir / vulkan_version / ARCHITECTURE
+
+
+def install_slang(install_dir: Path, vulkan_install_dir: Path):
+    with TemporaryDirectory() as tmp_dir:
+        # Download source
+        print(f"Downloading Slang {SLANG_VERSION} for {ARCHITECTURE}")
+        source_url = f"https://github.com/shader-slang/slang/releases/download/v{SLANG_VERSION}/slang-{SLANG_VERSION}-linux-{ARCHITECTURE}.tar.gz"
+        archive_path = Path(tmp_dir) / "slang.tar.gz"
+        extract_dir = Path(tmp_dir) / "slang"
+        urllib.request.urlretrieve(source_url, archive_path)
+
+        print(f"Extracting Slang {SLANG_VERSION}")
+        install_dir.mkdir(parents=True, exist_ok=True)
+        with tarfile.open(archive_path) as tar:
+            tar.extractall(extract_dir)
+
+        shutil.copytree(
+            extract_dir / "bin", vulkan_install_dir / "bin", dirs_exist_ok=True
+        )
+        shutil.copytree(
+            extract_dir / "include",
+            vulkan_install_dir / "slang" / "include",
+            dirs_exist_ok=True,
+        )
+        shutil.copytree(
+            extract_dir / "include",
+            vulkan_install_dir / "slang" / "include",
+            dirs_exist_ok=True,
+        )
+        shutil.copytree(
+            extract_dir / "lib", vulkan_install_dir / "lib", dirs_exist_ok=True
+        )
+        shutil.copytree(
+            extract_dir / "share", vulkan_install_dir / "share", dirs_exist_ok=True
+        )
 
 
 def install_module_file(
@@ -80,21 +129,26 @@ if __name__ == "__main__":
         help=f"Version of the Vulkan SDK to install. Defaults to the latest version ({VULKAN_SDK_VERSION})",
     )
     parser.add_argument(
-        "--skip-install",
+        "--skip-vulkan-install",
         action="store_true",
         default=False,
         help="Don't install anything. Only create module files.",
     )
     args = parser.parse_args()
 
-    install_dir = install_vulkan(
-        args.install_dir,
+    vulkan_install_dir = install_vulkan(
+        install_dir=args.install_dir,
         vulkan_version=args.vulkan_sdk_version,
-        dry_run=args.skip_install,
+        dry_run=args.skip_vulkan_install,
     )
+    if ARCHITECTURE == "aarch64" and SYSTEM == "Linux":
+        # Only required for aarch64 Linux, the official Vulkan SDK does already include Slang
+        install_slang(
+            install_dir=args.install_dir, vulkan_install_dir=vulkan_install_dir
+        )
     if args.module_dir is not None:
         install_module_file(
-            install_dir=install_dir,
+            install_dir=vulkan_install_dir,
             module_dir=args.module_dir,
             vulkan_version=args.vulkan_sdk_version,
             language=args.language,
